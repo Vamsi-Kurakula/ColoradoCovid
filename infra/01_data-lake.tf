da6035cc5ab75bd69df4f45ea2d68ff4cb26cf49
Vamsi-Kurakula <55557753+Vamsi-Kurakula@users.noreply.github.com>
2020-10-15 16:55:37 -0500

    Update terraform with new folder structure

--- infra/01_data-lake.tf
+++ infra/01_data-lake.tf
@@ -1,6 +1,6 @@
 variable "bucketname" {
     type = string
-    default = "cde-coloradocovid13"
+    default = "cde-coloradocovid231"
 }
 
 
@@ -12,21 +12,21 @@ resource "aws_s3_bucket" "coloradoCovid_bucket" {
 resource "aws_s3_bucket_object" "NYT_us" {
     bucket   = aws_s3_bucket.coloradoCovid_bucket.id
     acl      = "private"
-    key      = "Landing/NYT/us.csv"
+    key      = "Landing/NYT/All/us.csv"
     source   = "Data/NYT/us.csv" 
 }
 
 resource "aws_s3_bucket_object" "NYT_counties" {
     bucket   = aws_s3_bucket.coloradoCovid_bucket.id
     acl      = "private"
-    key      = "Landing/NYT/us-counties.csv"
+    key      = "Landing/NYT/Counties/us-counties.csv"
     source   = "Data/NYT/us-counties.csv" 
 }
 
 resource "aws_s3_bucket_object" "NYT_states" {
     bucket   = aws_s3_bucket.coloradoCovid_bucket.id
     acl      = "private"
-    key      = "Landing/NYT/us-states.csv"
+    key      = "Landing/NYT/States/us-states.csv"
     source   = "Data/NYT/us-states.csv" 
 }
 
@@ -37,9 +37,23 @@ resource "aws_s3_bucket_object" "JHU-TS" {
     source   = "Data/JHU/10-14-2020.csv" 
 }
 
-resource "aws_s3_bucket_object" "coloradoCovid" {
+resource "aws_s3_bucket_object" "coloradoCovid1" {
     bucket   = aws_s3_bucket.coloradoCovid_bucket.id
     acl      = "private"
-    key      = "Landing/ColoradoCovid/10-14-2020.csv"
-    source   = "Data/ColoradoCovid/10-14-2020.csv" 
+    key      = "Landing/ColoradoCovid/Hospital/covid19_hospital_data_2020-10-14.csv"
+    source   = "Data/ColoradoCovid/covid19_hospital_data_2020-10-14.csv" 
+}
+
+resource "aws_s3_bucket_object" "coloradoCovid2" {
+    bucket   = aws_s3_bucket.coloradoCovid_bucket.id
+    acl      = "private"
+    key      = "Landing/ColoradoCovid/Demographics/covid19_cases_demographics_tests_2020-10-14.csv"
+    source   = "Data/ColoradoCovid/covid19_cases_demographics_tests_2020-10-14.csv" 
+}
+
+resource "aws_s3_bucket_object" "reported" {
+    bucket   = aws_s3_bucket.coloradoCovid_bucket.id
+    acl      = "private"
+    key      = "Reported/.gitkeep"
+    source   = "Data/.gitkeep" 
 }
